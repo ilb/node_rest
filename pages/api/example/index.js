d445f5a82499897495f1f9c17d8ae64245cebd7f
MaximumQuiet <contact@maximumquiet.cyou>
2022-03-31 10:32:12 +0400

    Feature: use Response instead of default json response, hide routing

--- pages/api/example/index.js
+++ pages/api/example/index.js
@@ -2,6 +2,6 @@ import Example from '../queues/example';
 
 export default async (req, res) => {
   // some other work
-  const { redirectTo } = await Example.addTask({ contextValue: 'test' });
-  res.redirect(303, redirectTo);
+  const executeResponse = await Example.addTask({ contextValue: 'test' });
+  executeResponse(res);
 };
--- src/async/AsyncConnect.js
+++ src/async/AsyncConnect.js
@@ -1,53 +1,57 @@
 import nc from 'next-connect';
-import fetch from 'isomorphic-fetch';
 
 const AsyncConnect = ({ path, adapter, handler }) => {
   if (!path) throw new Error('API path should be defined');
-  if (!handler) throw new Error('Task handler should be defined')
+  if (!handler) throw new Error('Task handler should be defined');
 
   /**
    * Add new task to the queue
-   * @param {*} req 
+   * @param {*} req
    * @param {*} res
    */
   const addTask = async (req, res) => {
-    const uuid = taskManager.addTask({ handler })
+    const uuid = taskManager.addTask({ handler });
     res.redirect(303, `${path}/${uuid}`);
   };
 
   /**
    * Try to get task result
-   * @param {*} req 
-   * @param {*} res 
+   * @param {*} req
+   * @param {*} res
    */
   const getTask = async (req, res) => {
     const { uuid } = req.params;
     if (taskManager.isDone(uuid)) {
-      const data = await taskManager.getResult(uuid)
-      res.status(200).json({ status: 200, data, errors: []})
+      const response = await taskManager.getResult(uuid);
+
+      if (response instanceof Error) {
+        res.status(response.status).send(response.message);
+      }
+
+      if (response instanceof Response) {
+        for (const [header, value] of response.headers.entries()) {
+          res.setHeader(header, value);
+        }
+        res.status(200).send(response.content);
+      }
+
+      res.status(200).json({ data: response });
     } else {
-      res.setHeader('Refresh', `3;${path}/${uuid}`)
-      res.status(202).json({ status: 202, data: {}, errors: []})
+      res.setHeader('Refresh', `3;${path}/${uuid}`);
+      res.status(202).end();
     }
   };
 
   const nextApiHandler = nc({ attachParams: true }).post('/', addTask).get('/:uuid', getTask);
-  
+
   /**
    * Util method to incapsulate request call for adding new task
    * @param {Object} context task context
    */
   nextApiHandler.addTask = async (context) => {
-    const response = await fetch(`${path}`, {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(context),
-      redirect: 'follow'
-    })
-    const [_, redirectTo] = response.getHeader('Refresh').split(';');
-    return { redirectTo };
-  }
+    const uuid = taskManager.addTask({ handler });
+    return (res) => res.redirect(303, `${path}/${uuid}`);
+  };
 };
 
 export default AsyncConnect;
