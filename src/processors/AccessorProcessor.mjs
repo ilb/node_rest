ef0d622a5d6a974676ceb5d59005a99e3b5c782c
ssortia <alexkiyan.lug@gmail.com>
2022-06-09 20:45:39 +0300

    added debug logs

--- src/processors/AccessorProcessor.mjs
+++ src/processors/AccessorProcessor.mjs
@@ -4,6 +4,11 @@ export default class AccessorProcessor extends BaseProcessor {
   async buildResponse(result) {
     const accessorUrl = process.env.BASE_URL + this.context.req.url;
 
+    if (this.res.finished) {
+      console.log(this.res.statusCode);
+      console.log(this.res.getHeaders());
+    }
+
     if (result) {
       this.res.status(200).json(result);
     } else {
