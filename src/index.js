c36f9cc8aa40a5ef074dcd45c86d823ba4a26d56
DeniKungurov <denkungurow@yandex.ru>
2024-05-26 23:25:33 +0400

    import BaseProcessor

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ApiUsecase from './usecases/ApiUsecase.mjs';
 import FileUsecase from './usecases/FileUsecase.mjs';
 import AccessorUsecase from './usecases/AccessorUsecase.mjs';
 import router from './utils/router.mjs';
+import BaseProcessor from './processors/BaseProcessor.mjs';
 
-export { BaseUsecase, ApiUsecase, FileUsecase, AccessorUsecase, router };
+export { BaseUsecase, ApiUsecase, FileUsecase, AccessorUsecase,BaseProcessor, router };
